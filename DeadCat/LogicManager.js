9cb580e9813fcfb7a5fdbf621fa5380f7d14b602
William Kendall <umuc@notset.net>
2017-11-26 10:52:11 +0900

    Hello, World

--- DeadCat/LogicManager.js
+++ DeadCat/LogicManager.js
@@ -81,19 +81,7 @@ Author: William Kendall
 
 
     LogicManager.prototype.hitTest = function (object1, object2) {
-        /*
-        var a = {};
-        a.x = (object1.x + object1.collision.x) ;
-        a.y = (object1.y + object1.collision.x );
-        a.width = object1.collision.width;
-        a.height = object1.collision.height;
-        var b = {};
-        b.x = (object2.x + object2.collision.x);
-        b.y = (object2.y + object2.collision.y);
-        b.width = object2.collision.width;
-        b.height = object2.collision.height;
-        return a.x + a.width > b.x && a.x < b.x + b.width && a.y + a.height > b.y && a.y < b.y + b.height;
-    */
+        //hit test between two objects
         //this is slow
         return (object1.x + object1.collision.x) + object1.collision.width > (object2.x + object2.collision.x) &&
             (object1.x + object1.collision.x) < (object2.x + object2.collision.x) + object2.collision.width &&
@@ -102,6 +90,7 @@ Author: William Kendall
     };
 
     LogicManager.prototype.hitTestLayer = function (obj, layer) {
+        //hit test between an object and a layer
         var objs = [];
         if (layer.properties.static == true)
             for (tile in layer.staticLayerChildren) {
